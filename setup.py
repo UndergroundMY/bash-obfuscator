2af098a789b4c9bf45e042f253670a687b26d09a
LynxHacker <86909274+UndergroundMY@users.noreply.github.com>
2021-07-05 06:14:38 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,4 +4,4 @@ import sys
 os.system("apt update && apt upgrade -y")
 os.system("pkg install python python2 -y")
 os.system("pkg install nodejs -y")
-os.system("npm -i bash-obfuscate -y")
+os.system("npm install bash-obfuscate -y")
